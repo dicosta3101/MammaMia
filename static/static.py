621b4bcc75f958e3336c0c323f2362e3582b1e6c
dicosta3101 <francesco.dicostanzo3101@gmail.com>
2025-10-04 15:42:27 +0200

    Update static.py

--- static/static.py
+++ static/static.py
@@ -1,5 +1,11 @@
 HTML = """
-howtosetupmammamiaaaddininorder to creater a own server""""""
+Don’t care about them all
+Cause all I want is to be loved
+And all I care about is you
+You’re stuck on me like a tattoo
+No, I don’t care about the pain
+I’ll walk through fire and through rain
+"""
 HTML = """
 <!DOCTYPE html>
 <html>
